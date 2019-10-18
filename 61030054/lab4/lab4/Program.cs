5cd818e9a4d1dbb923686b84fbe1b6b07c54b5ba
“https://github.com/61030054-Computer-Lab-1/Week-03.git <suprakron15@gmail.comgit config -- global user.email suprakron15@gmail.com>
2019-10-18 18:10:25 +0700

    add floder Week-04.md

---
+++ 61030054/lab4/lab4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string name = Console.ReadLine();
+            string lastname = Console.ReadLine();
+            int id = Convert.ToInt32(Console.ReadLine());
+            double gpa = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("name :"+name);
+            Console.WriteLine("lastname :"+lastname);
+            Console.WriteLine("id :"+id);
+            Console.WriteLine("gpa :"+gpa);
+            Console.ReadKey();
+        }
+    }
+}
