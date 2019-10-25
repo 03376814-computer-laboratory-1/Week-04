2c5b7e9a1b273ed7972816d3f718c227d550fd0e
kkkanitar <khanita03272@gmail.com>
2019-10-25 16:42:17 +0700

    456u8899

--- 61030006/lab4/lab04/lab04/Program.cs
+++ 61030006/lab4/lab04/lab04/Program.cs
@@ -10,7 +10,7 @@ namespace lab04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0,-10:x}", 65535);  // (x = lower case)
+            Console.WriteLine("{0,-10:X}", 65535);  // (X = upper case)
         }
     }
 }
