513bef053b45cd9b91b33c1d90142a86197b06f0
kkkanitar <khanita03272@gmail.com>
2019-10-25 15:58:22 +0700

    jiok

---
+++ 61030006/lab4/lab4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 9);
+            Console.WriteLine(randomNumber);
+            int x = Convert.ToInt32(Console.ReadLine());
+            bool y = randomNumber == x;
+            Console.WriteLine("{0}", y);
+            Console.WriteLine(y);
+            Console.ReadKey();
+
+            Console.WriteLine($"Number random : {randomNumber}");
+
+
+        }
+    }
+}
