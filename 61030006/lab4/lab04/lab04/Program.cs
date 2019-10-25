ddc3421c6ac1cf784b8e85a82da5c9db7bc8e7f3
kkkanitar <khanita03272@gmail.com>
2019-10-25 16:36:00 +0700

    34

--- 61030006/lab4/lab04/lab04/Program.cs
+++ 61030006/lab4/lab04/lab04/Program.cs
@@ -10,7 +10,7 @@ namespace lab04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{2:d} {0:d} {1:d}", 1, 2, 3);
+            Console.WriteLine("Hello " + "World");
         }
     }
 }
