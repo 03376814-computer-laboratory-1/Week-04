bd14906702fc8ccc5f2ef10e97b9b10443c8fc24
janezthh <61030009@kmitl.ac.th>
2019-10-25 14:40:56 +0700

    Add floder 61030009

---
+++ 61030009/LAB0004/LAB0004/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace thruthTable
+{
+    using System;
+    public class intergerTest
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20, x = 5, y = 2;
+            decimal C;
+            Console.WriteLine("{0}+{1} = {2}", a, b, C);
+        }
+    }
+}
